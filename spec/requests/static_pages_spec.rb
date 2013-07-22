fcb8d2ccdc122e165fb64fbb11c7abe0199f6e7a
Jan van der Pas <jvanderpas@digidentity.eu>
2013-07-22 09:45:24 +0200

    Add title test to rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  def should_have_title
+     have_title("Ruby on Rails Tutorial Sample App | #{title}")
+  end
+
+  def let(name)
+    @lets ||= {}
+    @lets[name] ||= iets
+  end
+
   describe "Home page" do
+    let(:title) { 'Home' }
     it "does have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
@@ -9,10 +19,11 @@ describe "StaticPages" do
 
     it "does have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to should_have_title
     end
   end
   describe "Help page" do
+    let(:title) { 'Help' }
     it "does have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
@@ -20,10 +31,11 @@ describe "StaticPages" do
 
     it "does have right title" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')    
+      expect(page).to should_have_title   
     end
   end
   describe "About page" do
+    let(:title) { 'About' }
     it "does have the content 'about'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
@@ -31,13 +43,14 @@ describe "StaticPages" do
 
     it "does have right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')    
+      expect(page).to should_have_title    
     end
   end
   describe "Contact page" do
+    let(:title) { 'Contact' }
     it "does have right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+      expect(page).to should_have_title
     end
   end
 end
