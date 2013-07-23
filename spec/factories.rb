37743945d3dfaae5ad924d7bf28736fd108e5b6c
Jan van der Pas <jvanderpas@digidentity.eu>
2013-07-23 15:42:55 +0200

    Add FactoyGirl, User factory and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name    "Jan"
+    email   "user@example.com"
+    password "welkom01"
+    password_confirmation "welkom01"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,18 @@ describe "UserPages" do
 
   subject { page }
   
-  describe "signup page" do
+  describe "Signup page" do
     before { visit sign_up_path }
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+  end
+
+  describe "Profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(full_title(user.name)) }
   end
 end
