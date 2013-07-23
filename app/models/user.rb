eb3cd07779e5ac1eb18846ee2b3c4f221fac799f
Jan van der Pas <jvanderpas@digidentity.eu>
2013-07-23 11:15:51 +0200

    Add name and email to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true, length: { maximum: 50 }
 end
